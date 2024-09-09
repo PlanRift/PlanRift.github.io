d06bc8dc8d02fd3a0bc92fb1278b5d070941223c
PlanRift <dzikramufidatovid@gmail.com>
2024-09-09 22:33:10 +0700

    lil update for animations

--- photo/allPic/allPic.js
+++ photo/allPic/allPic.js
@@ -4,7 +4,13 @@ document.addEventListener("DOMContentLoaded", function () {
   CustomEase.create("outSmooth", "M0,0 C0.139,0.336 0.202,1 1,1 ");
   
   gsap.timeline()
-    .to(".overlay", { duration: 2, opacity: 0, ease: "outSmooth" }, "+=0.5");
+  .to(".overlay", { duration: 2, opacity: 0, ease: "outSmooth" }, "+=0.5")
+  .from(".navbar",{ duration: 1.5, y:-100, ease:"outSmooth"}, "-=2")
+  .from(".main-header",{ duration:1.5, x:-1000, ease:"outSmooth"}, "-=2")
+  .from(".second-header",{ duration:1.5, x:-1000, ease:"outSmooth"}, "-=1.9")
+  .from(".header", {
+    backgroundPosition: "200% 50%",opacity:0, duration: 2, ease:"outSmooth"}, "=-2");
+
 
     const allJsonUrls = [
       'https://raw.githubusercontent.com/PlanRift/driveImagesToHTML/main/pets.JSON',

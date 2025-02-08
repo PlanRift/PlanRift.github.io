8e046faec68c8c65d459508a17a616b1e0ad0e9b
PlanRift <dzikramufidatovid@gmail.com>
2025-02-08 13:22:15 +0700

    photography responsive, new feature

--- js/photography.js
+++ js/photography.js
@@ -9,37 +9,54 @@ document.addEventListener("DOMContentLoaded", function () {
     .from(".main-header", { duration: 1.5, x: 1000, ease: "outSmooth" }, "-=2")
     .from(".second-header", { duration: 1.5, x: 1000, ease: "outSmooth" }, "-=1.9")
     .from(".header", {
-     opacity: 0, duration: 2, ease: "outSmooth"
+      opacity: 0, duration: 2, ease: "outSmooth"
     }, "=-2");
-    
-    const allJsonUrls = [
-      'https://raw.githubusercontent.com/PlanRift/driveImagesToHTML/main/pets.JSON',
-      'https://raw.githubusercontent.com/PlanRift/driveImagesToHTML/main/street.JSON'
-    ];
-    
-    Promise.all(allJsonUrls.map(url => fetch(url).then(response => response.json())))
-      .then(allData => {
-        const rows = [
-          [allData[1][4], allData[1][1], allData[0][7]],
-          [allData[1][20], allData[1][38]],
-          [allData[1][30], allData[1][31], allData[1][33]],
-          [allData[0][6], allData[1][9], allData[1][8], allData[1][7]],
-          [allData[1][6], allData[0][4]],
-          [allData[0][1], allData[1][21], allData[1][5]]
-        ];
-    
-        // Loop through each row to insert images
-        rows.forEach((row, index) => {
-          const rowElement = document.getElementById(`row-${index + 1}`);
-          row.forEach(item => {
-            const img = document.createElement('img');
-            img.src = item.url;
-            img.alt = item.name;
-            rowElement.appendChild(img);
-          });
+
+  const allJsonUrls = [
+    "https://raw.githubusercontent.com/PlanRift/driveImagesToHTML/main/pets.JSON",
+    "https://raw.githubusercontent.com/PlanRift/driveImagesToHTML/main/street.JSON"
+  ];
+
+  Promise.all(allJsonUrls.map(url => fetch(url).then(response => response.json())))
+    .then(allData => {
+      const rows = [
+        [allData[1][4], allData[1][1], allData[0][7]],
+        [allData[1][20], allData[1][38]],
+        [allData[1][30], allData[1][31], allData[1][33]],
+        [allData[0][6], allData[1][9], allData[1][8], allData[1][7]],
+        [allData[1][6], allData[0][4]],
+        [allData[0][1], allData[1][21], allData[1][5]]
+      ];
+
+      rows.forEach((row, index) => {
+        const rowElement = document.getElementById(`row-${index + 1}`);
+        row.forEach(item => {
+          const img = document.createElement("img");
+          img.src = item.url;
+          img.alt = item.name;
+          rowElement.appendChild(img);
         });
-      })
-      .catch(error => console.error('Error fetching JSON:', error));
+      });
+    })
+    .catch(error => console.error("Error fetching JSON:", error));
+
+    document.addEventListener("click", function (e) {
+      if (e.target.tagName === "IMG") { 
+        const mainImage = e.target.src; 
+        const imageModal = document.getElementById("image-modal");
+        const content = document.getElementById("content-modal");
+        const modalImage = document.getElementById("modal-image"); 
+        modalImage.src = mainImage; 
+        content.classList.add("open-content");
+        imageModal.classList.add("open-modal");
+      } else if (e.target.classList.contains("modal")) {
+        const content = document.getElementById("content-modal");
+        const imageModal = document.getElementById("image-modal");
+        content.classList.remove("open-content");
+        imageModal.classList.remove("open-modal");
+      }
+    });
+    
     
 
   const mainHeader = document.querySelector(".gohome");
@@ -50,7 +67,7 @@ document.addEventListener("DOMContentLoaded", function () {
         opacity: 1,
         ease: "outSmooth",
       }).then(() => {
-        window.location.href = "../index.html"; // Redirect to the home page
+        window.location.href = "../";
       });
     });
   }
